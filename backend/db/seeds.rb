7a3b167b4cedbd345339ff1c0fe4d747519b8d42
Josua Schmid <josua.schmid@renuo.ch>
2018-04-22 00:30:05 +0200

    Skip confirmation

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,6 +1,8 @@
 include FactoryBot::Syntax::Methods
 
-user = User.create!(nickname: 'sadi', email: 'sadi@noproblan.ch', password: '12345678')
+user = User.new(nickname: 'sadi', email: 'sadi@noproblan.ch', password: '12345678')
+user.skip_confirmation!
+user.save!
 
 create(:lan, :past, name: 'v4.5', tickets: [build(:ticket, user: user)])
 create(:lan, :now, name: 'v31.3', tickets: [build(:ticket, user: user)])
