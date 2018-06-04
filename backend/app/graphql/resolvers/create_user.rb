67750a86001ec5f9dde41f6d9316e04937cc2120
Josua Schmid <josua.schmid@renuo.ch>
2018-06-05 00:03:07 +0200

    Rubocop autocorrect

--- backend/app/graphql/resolvers/create_user.rb
+++ backend/app/graphql/resolvers/create_user.rb
@@ -1,22 +1,24 @@
-class Resolvers::CreateUser < GraphQL::Function
-  AuthProviderInput = GraphQL::InputObjectType.define do
-    name 'AuthProviderSignupData'
+module Resolvers
+  class CreateUser < GraphQL::Function
+    AuthProviderInput = GraphQL::InputObjectType.define do
+      name 'AuthProviderSignupData'
 
-    argument :email, Types::AuthProviderEmailInput
-  end
+      argument :email, Types::AuthProviderEmailInput
+    end
 
-  argument :nickname, !types.String
-  argument :authProvider, AuthProviderInput
+    argument :nickname, !types.String
+    argument :authProvider, AuthProviderInput
 
-  type Types::UserType
+    type Types::UserType
 
-  def call(_obj, args, _ctx)
-    User.create!(
-      nickname: args[:nickname],
-      email: args[:authProvider]&.[](:email)&.[](:email),
-      password: args[:authProvider]&.[](:email)&.[](:password)
-    )
-  rescue ActiveRecord::RecordInvalid => e
-    GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
+    def call(_obj, args, _ctx)
+      User.create!(
+        nickname: args[:nickname],
+        email: args[:authProvider]&.[](:email)&.[](:email),
+        password: args[:authProvider]&.[](:email)&.[](:password)
+      )
+    rescue ActiveRecord::RecordInvalid => e
+      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
+    end
   end
 end
